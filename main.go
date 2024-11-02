b035b3da7e8e00bb1f17a0772729a5b854f1a4b5
Siddhant Shrivastav <sidshrivastav1002@gmail.com>
2024-11-02 12:03:48 +0530

    feat: added config in main

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"go-starter/config"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there %s", r.URL.Path[1:])
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify the environment (dev/prod)")
+		return
+	}
+	env := os.Args[1]
+	cfg := config.LoadConfig(env)
+
+	http.HandleFunc("/", handler)
+	// Start HTTP server
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
